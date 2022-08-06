832e645d5cbde18322262b265e780e154447ddd6
LH <lh@LHs-MacBook-Air.local>
2022-08-06 12:52:57 +0300

    Updating number of copies when a book is borrowd

--- library/booksTransaction/views.py
+++ library/booksTransaction/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth.models import User
 # Create your views here.
 def borrow_book(request,book_id,user_id):
     book=Book.objects.get(pk=book_id)
+    book.number_of_copies-=1
+    book.save()
     member=User.objects.get(id=user_id)
     borrow=Borrowing.objects.create(book=book,user=member)
     return render(request,"borrowed.html")
