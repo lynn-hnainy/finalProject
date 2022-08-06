b12b62c1c23119215160d4b967cfd9637fed54eb
LH <lh@LHs-MacBook-Air.local>
2022-08-06 13:06:59 +0300

    Check if book is already borrowed

--- library/booksTransaction/views.py
+++ library/booksTransaction/views.py
@@ -1,14 +1,18 @@
 from urllib import request
 from django.shortcuts import render,redirect
 from books.models import Book
-from members.models import Member
+from django.contrib import messages
 from .models import Borrowing
 from django.contrib.auth.models import User
 # Create your views here.
 def borrow_book(request,book_id,user_id):
-    book=Book.objects.get(pk=book_id)
-    book.number_of_copies-=1
-    book.save()
-    member=User.objects.get(id=user_id)
-    borrow=Borrowing.objects.create(book=book,user=member)
-    return render(request,"borrowed.html")
+    if Book.objects.filter(pk=book_id).exists() and User.objects.filter(id=user_id).exists():
+        messages.error(request, 'Book already borrowed')
+        return redirect('home')
+    else:
+        book=Book.objects.get(pk=book_id)
+        book.number_of_copies-=1
+        book.save()
+        member=User.objects.get(id=user_id)
+        borrow=Borrowing.objects.create(book=book,user=member)
+        return render(request,"borrowed.html")
