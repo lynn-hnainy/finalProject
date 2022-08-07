dd2f082872383547287581e537be872e76cae4e8
LH <lh@LHs-MacBook-Air.local>
2022-08-07 08:37:30 +0300

    Fix checking if book already borrowed

--- library/booksTransaction/views.py
+++ library/booksTransaction/views.py
@@ -6,14 +6,14 @@ from .models import Borrowing
 from django.contrib.auth.models import User
 # Create your views here.
 def borrow_book(request,book_id,user_id):
-    if Book.objects.filter(pk=book_id).exists() and User.objects.filter(id=user_id).exists():
+    book=Book.objects.get(pk=book_id)
+    member=User.objects.get(id=user_id)
+    if Borrowing.objects.filter(book=book) and Borrowing.objects.filter(user=member).exists():
         messages.error(request, 'Book already borrowed')
         return redirect('home')
     else:
-        book=Book.objects.get(pk=book_id)
         book.number_of_copies-=1
         book.save()
-        member=User.objects.get(id=user_id)
         borrow=Borrowing.objects.create(book=book,user=member)
         return render(request,"borrowed_successfully.html")
 
