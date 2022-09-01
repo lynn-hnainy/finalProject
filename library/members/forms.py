dece46fd98785b24a7814a31ed778c51fc00d8d9
LH <lh@LHs-MacBook-Air.local>
2022-09-01 09:44:47 +0300

    Fixing register form:Adding labels

--- library/members/forms.py
+++ library/members/forms.py
@@ -5,20 +5,20 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class UserForm(UserCreationForm):
-    first_name=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control my-2','placeholder':'First Name'}))
-    last_name=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control my-2','placeholder':'Last Name'}))
-    username=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control my-2','placeholder':'Enter Username'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control my-2','placeholder':'Enter Email'}))
-    password1=forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control my-2','placeholder':'Password'}))
-    password2=forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control my-2','placeholder':'Confirm Password'}))
+    first_name=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control my-2','placeholder':'John'}))
+    last_name=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control my-2','placeholder':'Doe'}))
+    username=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control my-2','placeholder':'john.doe'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control my-2','placeholder':'john@doe.com'}))
+    password1=forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control my-2','placeholder':'********'}))
+    password2=forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control my-2','placeholder':'********'}))
     class Meta:
         model = User
         fields = ('first_name','last_name','username','email','password1','password2')
 
 class MemberForm(forms.ModelForm):
-    phone=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control my-2','placeholder':'Phone Number'}))
-    birthdaydate=forms.DateField(widget=forms.DateInput(attrs={'class':'form-control my-2','placeholder':'Birthday'}))
-    address=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control my-2','placeholder':'Address'}))
+    phone=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control my-2','placeholder':'03000000'}))
+    birthdaydate=forms.DateField(widget=forms.DateInput(attrs={'class':'form-control my-2','placeholder':'MM-DD-YYYY'}))
+    address=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control my-2','placeholder':'Saida'}))
     class Meta:
         model = Member
         fields = ('phone', 'birthdaydate', 'address')
